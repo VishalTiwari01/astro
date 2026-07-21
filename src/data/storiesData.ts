d75393b023db005a3e12c126d2c359e4610506bb
fastigo-ai <fastigo.in@gmail.com>
2026-07-21 12:02:03 +0530

    Revert "fix: updated StoriesSlider theme colors, font, images and scrolling"
    This reverts commit b28d0404c786866ae9a0cd344d2dc4b66bbafa33.

--- src/data/storiesData.ts
+++ src/data/storiesData.ts
@@ -3,9 +3,8 @@ export const storiesData = [
     name: "Mr. & Mrs. Patil",
     city: "Kolhapur",
     rating: 5,
-    quote: "Krishna Coming has helped us experience a calmer journey.",
+    quote: "AstroBaby has helped us experience a calmer journey.",
     videoId: "vEltMP4qvhw",
-    img: "https://www.krishnacoming.com/template/front/krishnaAssest/img/experiences/experience_1727683696.jpg"
   },
   {
     name: "Mrs. Preeti Shree",
@@ -13,7 +12,6 @@ export const storiesData = [
     rating: 5,
     quote: "The expert guidance was exactly what we needed during this time.",
     videoId: "5YhDhV9Eq4M",
-    img: "https://www.krishnacoming.com/template/front/krishnaAssest/img/experiences/experience_1724400221.jpg"
   },
   {
     name: "Mr. & Mrs. Chaturkar",
@@ -21,30 +19,5 @@ export const storiesData = [
     rating: 5,
     quote: "We feel more confident and connected as a family now.",
     videoId: "Q8V0cuHGBx4",
-    img: "https://www.krishnacoming.com/template/front/krishnaAssest/img/experiences/experience_1724396585.jpg"
-  },
-  {
-    name: "Mr. & Mrs. Sinha",
-    city: "Bangalore",
-    rating: 5,
-    quote: "Highly recommend for a healthy pregnancy experience.",
-    videoId: "wGaFK-N_-eo",
-    img: "https://www.krishnacoming.com/template/front/krishnaAssest/img/experiences/experience_1723106769.jpg"
-  },
-  {
-    name: "Mr. & Mrs. Dubey",
-    city: "Gwalior",
-    rating: 5,
-    quote: "An amazing companion throughout the 9 months.",
-    videoId: "tkXuMEOJCKo",
-    img: "https://www.krishnacoming.com/template/front/krishnaAssest/img/experiences/experience_1698234576.jpg"
-  },
-  {
-    name: "Mr. & Mrs. Jalan",
-    city: "Delhi",
-    rating: 5,
-    quote: "Very informative and stress-relieving sessions.",
-    videoId: "6MgQ1sZ_tlI",
-    img: "https://www.krishnacoming.com/template/front/krishnaAssest/img/experiences/experience_1697802459.jpg"
   },
 ];
